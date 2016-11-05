fixed basis conversion issues by forcing omega to be in radians everywhere

radvel/basis.py
@@ -90,7 +90,7 @@ class Basis(object):
                 per = _getpar('per')
                 tp = _getpar('tp')
                 e = _getpar('e')
-                w = _getpar('w')
+                w = _getpar('w')   
                 k = _getpar('k')
                 
             if self.name == 'per tc e w k':
@@ -132,7 +132,7 @@ class Basis(object):
             _setpar('per', per)
             _setpar('tp', tp)
             _setpar('e', e)
-            _setpar('w', np.degrees(w))
+            _setpar('w', w)
             _setpar('k', k)
 
 
@@ -174,7 +174,17 @@ class Basis(object):
             
 
             if newbasis == 'per tc e w k':
-                pass
+                per = _getpar('per')
+                e = _getpar('e')
+                w = _getpar('w')
+                tp = _getpar('tp')
+                
+                _setpar('tc', timeperi_to_timetrans(tp, per, e, w) )
+                _setpar('w', w )
+
+                if not kwargs.get('keep', True):
+                    _delpar('tp')
+
             
             if newbasis == 'per tc secosw sesinw logk':
                 per = _getpar('per')

radvel/kepler.py
@@ -16,7 +16,7 @@ def rv_drive(t, orbel, use_C_kepler_solver=cext):
         t (array of floats): times of observations
         orbel (array of floats): [per, tp, e, om, K].\
             Omega is expected to be\
-            in degrees
+            in radians
         use_C_kepler_solver (bool): (default: True) If \
             True use the Kepler solver written in C, else \
             use the Python/NumPy version.
@@ -28,7 +28,6 @@ def rv_drive(t, orbel, use_C_kepler_solver=cext):
     
     # unpack array
     per, tp, e, om, k = orbel
-    om = om / 180 * np.pi
     
     # Error checking
     if e == 0.0: