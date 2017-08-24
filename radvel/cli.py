d6cfcafa2830890c63e03039b3569380341578bf
Evan Sinukoff <evan@evan1.local>
2017-08-24 11:10:01 -0400

    Fixed crash when outputdir does not exist

--- radvel/cli.py
+++ radvel/cli.py
@@ -85,7 +85,7 @@ Number of ensembles. Will be run in parallel on separate CPUs [8]
     psr_mcmc.set_defaults(func=radvel.driver.mcmc)
 
 
-    # Derive hysical parameters
+    # Derive physical parameters
     psr_physical = subpsr.add_parser(
         'derive', parents=[psr_parent],
         description="Multiply MCMC chains by physical parameters. MCMC must"
@@ -146,7 +146,7 @@ Number of ensembles. Will be run in parallel on separate CPUs [8]
         args.outputdir = outdir
             
     if not os.path.isdir(args.outputdir):
-        os.mkdir(outdir)
+        os.mkdir(args.outputdir)
         
     args.func(args)
 
