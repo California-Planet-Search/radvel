206008deee349c4b1b20f1919f44eb92a7e969b5
sblunt <sarah.blunt.3@gmail.com>
2017-10-20 10:16:33 -0700

    replace tabs w/spaces, length->gp_length

--- radvel/gp.py
+++ radvel/gp.py
@@ -1,7 +1,6 @@
 import sys
 import radvel
 import scipy
-from scipy.linalg import cho_factor, cho_solve
 from abc import ABCMeta, abstractproperty, abstractmethod
 import numpy as np
 
@@ -29,89 +28,91 @@ class Kernel(ABC):
 
     @abstractproperty
     def name(self):
-    	pass
+        pass
 
     @abstractmethod
-    def compute_covmatrix(self, X1, X2):   
-	"""Abstract method for computing a covariance matrix."""
-    	pass
+    def compute_covmatrix(self, X1, X2):
+        pass
+    """
+    Abstract method for computing a covariance matrix."""
 
     def add_diagonal_errors(cls, errors):
-    	cls.covmatrix += (errors**2.) * np.identity(cls.covmatrix.shape[0])
+        cls.covmatrix += (errors**2.) * np.identity(cls.covmatrix.shape[0])
 
 
 class SqExpKernel(Kernel):
 
-	@property
-	def name(self):
-		return "SqExp"
+    @property
+    def name(self):
+        return "SqExp"
 
-	def __init__(self, params, covmatrix=None):
-		self.covmatrix = covmatrix
-		for par in params:
-			if par.startswith('length'):
-				self.length = params[par].value
-			if par.startswith('amp'):
-				self.amp = params[par].value
+    def __init__(self, params, covmatrix=None):
+        self.covmatrix = covmatrix
+        for par in params:
+            if par.startswith('gp_length'):
+                self.length = params[par].value
+            if par.startswith('gp_amp'):
+                self.amp = params[par].value
 
-	def compute_covmatrix(self, X1, X2):
-		dist = scipy.spatial.distance.cdist(X1, X2, 'sqeuclidean')
-		K = scipy.matrix(self.amp**2 * scipy.exp(-dist/(2.*self.length**2)))
-		self.covmatrix = K
+    def __repr__(self):
+    	return "SqExp Kernel with length: {}, amp: {}".format(self.length, self.amp)
+
+    def compute_covmatrix(self, X1, X2):
+        dist = scipy.spatial.distance.cdist(X1, X2, 'sqeuclidean')
+        K = scipy.matrix(self.amp**2 * scipy.exp(-dist/(2.*self.length**2)))
+        self.covmatrix = K
 
 
 class PerKernel(Kernel):
 
-	@property
-	def name(self):
-		return "Per"
+    @property
+    def name(self):
+        return "Per"
 
-	def __init__(self, params, covmatrix=None):
-		self.covmatrix = covmatrix
-		for par in params:
-			if par.startswith('length'):
-				self.length = params[par].value
-			if par.startswith('amp'):
-				self.amp = params[par].value
-			if par.startswith('length'):
-				self.length = params[par].value
+    def __init__(self, params, covmatrix=None):
+        self.covmatrix = covmatrix
+        for par in params:
+            if par.startswith('gp_length'):
+                self.length = params[par].value
+            if par.startswith('gp_amp'):
+                self.amp = params[par].value
+            if par.startswith('gp_per'):
+                self.per = params[par].value
 
-	def compute_covmatrix(self, X1, X2):
-		dist = scipy.spatial.distance.cdist(X1, X2, 'euclidean')
-		K = scipy.matrix(self.amp**2 * scipy.exp(-2.*np.sin(np.pi*dist/self.per)**2.
-												 /(self.length**2)))
+    def __repr__(self):
+    	return "Per Kernel with length: {}, amp: {}, per: {}".format(self.length, 
+                                                                     self.amp, self.per)
+
+    def compute_covmatrix(self, X1, X2):
+        dist = scipy.spatial.distance.cdist(X1, X2, 'euclidean')
+        K = scipy.matrix(self.amp**2 * scipy.exp(-2.*np.sin(np.pi*dist/self.per)**2.
+                                                 /(self.length**2)))
 
 class QuasiPerKernel(Kernel):
-	
-	@property
-	def name(self):
-		return "QuasiPer"
 
-	def __init__(self, params, covmatrix=None):
-		self.covmatrix = covmatrix
-		for par in params:
-			if par.startswith('length'):
-				self.length = params[par].value
-			if par.startswith('amp'):
-				self.amp = params[par].value
-			if par.startswith('length'):
-				self.length = params[par].value
+    @property
+    def name(self):
+        return "QuasiPer"
 
-	def compute_covmatrix(self, X1, X2):
-		dist_p = scipy.spatial.distance.cdist(X1, X2, 'euclidean')
-		dist_se = scipy.spatial.distance.cdist(X1, X2, 'sqeuclidean')
-		K = scipy.matrix(self.amp**2
-						 * scipy.exp(-2.*np.sin(np.pi*dist_p/self.per)**2.
-						 			 / (self.length**2))
-						 * scipy.exp(-dist_se/(2.*self.length**2)))
+    def __init__(self, params, covmatrix=None):
+        self.covmatrix = covmatrix
+        for par in params:
+            if par.startswith('gp_length'):
+                self.length = params[par].value
+            if par.startswith('gp_amp'):
+                self.amp = params[par].value
+            if par.startswith('gp_per'):
+                self.per = params[par].value
 
-if __name__ == "__main__":
-	x = scipy.matrix([1,2]).T
-	p = SqExpKernel(1,2)
-	p.compute_covmatrix(x,x)
-	print(p.covmatrix)
-	p.add_diagonal_errors(1.)
-	print(p.covmatrix)
-	p.add_diagonal_errors(np.array([1.,2.]))
-	print(p.covmatrix)
+    def __repr__(self):
+    	return "QuasiPer Kernel with length: {}, amp: {}, per: {}".format(self.length, 
+                                                                          self.amp, self.per)
+
+    def compute_covmatrix(self, X1, X2):
+        dist_p = scipy.spatial.distance.cdist(X1, X2, 'euclidean')
+        dist_se = scipy.spatial.distance.cdist(X1, X2, 'sqeuclidean')
+        K = scipy.matrix(self.amp**2
+                         * scipy.exp(-2.*np.sin(np.pi*dist_p/self.per)**2.
+                                     / (self.length**2))
+                         * scipy.exp(-dist_se/(2.*self.length**2)))
 
