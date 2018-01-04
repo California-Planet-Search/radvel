6555b8d3500244fa91e69e9cdad476e61447c72a
BJ Fulton <bjfultn@gmail.com>
2018-01-03 17:00:55 -0800

    restoring all tests

--- radvel/tests/test_api.py
+++ radvel/tests/test_api.py
@@ -46,14 +46,14 @@ def _standard_run(setupfn):
     radvel.driver.report(args)
 
         
-def _test_k2(setupfn='example_planets/epic203771098.py'):
+def test_k2(setupfn='example_planets/epic203771098.py'):
     """
     Run through K2-24 example
     """    
     _standard_run(setupfn)
 
 
-def _test_hd(setupfn='example_planets/HD164922.py'):
+def test_hd(setupfn='example_planets/HD164922.py'):
     """
     Check multi-instrument fit
     """
@@ -68,7 +68,7 @@ def _test_hd(setupfn='example_planets/HD164922.py'):
     radvel.driver.plots(args)
 
 
-def _test_k2131(setupfn='example_planets/k2-131.py'):
+def test_k2131(setupfn='example_planets/k2-131.py'):
     """
     Check multi-instrument fit
     """
@@ -82,7 +82,7 @@ def _test_k2131(setupfn='example_planets/k2-131.py'):
     radvel.driver.plots(args)
 
 
-def _test_basis():
+def test_basis():
     """
     Test basis conversions
     """
@@ -120,7 +120,7 @@ def _test_basis():
 {}, {} != {}".format(par, before, after)
 
 
-def _test_kernels():
+def test_kernels():
     """
     Test basic functionality of all GP kernels
     """
@@ -140,7 +140,7 @@ def _test_kernels():
 
 
 
-def _test_kepler():
+def test_kepler():
     """
     Profile and test C-based Kepler solver
     """
