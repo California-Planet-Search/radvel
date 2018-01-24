598f26fa987db70602a7b2e80a90c8fa83227085
BJ Fulton <bjfultn@gmail.com>
2018-01-24 15:34:31 -0800

    adding more default plot colors

--- radvel/plotting.py
+++ radvel/plotting.py
@@ -58,7 +58,7 @@ def _mtelplot(x, y, e, tel, ax, telfmts={}):
     """
     lw = 1.0
 
-    default_colors = ['orange', 'purple', 'magenta', 'pink', 'blue', 'red', 'grey']
+    default_colors = ['orange', 'purple', 'magenta', 'pink', 'blue', 'grey']
     ci = 0
     
     utel = np.unique(tel)
