812d64e19bd4cf04f2c05aece06a9729b6ff8631
sblunt <sarah.blunt.3@gmail.com>
2018-02-09 10:14:52 -0800

    Error Messages++ and celerite Development++
    - added specific error messages to GP code.
    - more celerite development.

--- radvel/gp.py
+++ radvel/gp.py
@@ -4,11 +4,15 @@ import scipy
 from scipy import spatial
 import abc
 import numpy as np
+import celerite
 
 # implemented kernels & list of their associated hyperparameters
 KERNELS = {"SqExp":['gp_length','gp_amp'],
            "Per": ['gp_per','gp_length','gp_amp'],
-           "QuasiPer": ['gp_per','gp_perlength','gp_explength','gp_amp']}
+           "QuasiPer": ['gp_per','gp_perlength','gp_explength','gp_amp'],
+           "Celerite": []}
+
+           # TODO: update kernel params for celerite
 
 if sys.version_info[0] < 3:
     ABC = abc.ABCMeta('ABC', (), {})
@@ -71,13 +75,16 @@ class SqExpKernel(Kernel):
                 self.hparams['gp_amp'] = hparams[par]
 
         assert len(hparams) == 2, \
-        "KERNEL ERROR: incorrect number of hyperparameters passed to SqExp Kernel "
+        "SqExpKernel requires exactly 2 hyperparameters with names 'gp_length*' and 'gp_amp*'."
 
         try:
             self.hparams['gp_length'].value
             self.hparams['gp_amp'].value
-        except:
-            print("KERNEL ERROR: SqExp Kernel requires hyperparameters 'gp_length*' and 'gp_amp*'")
+        except KeyError:
+            raise KeyError("SqExpKernel requires hyperparameters 'gp_length*' and 'gp_amp*'.")
+        except AttributeError:
+            raise AttributeError("SqExpKernel requires dictionary of Parameter objects as input")
+
 
 
     def __repr__(self):
@@ -131,15 +138,16 @@ class PerKernel(Kernel):
                 self.hparams['gp_per'] = hparams[par]
 
         assert len(hparams) == 3, \
-        "KERNEL ERROR: incorrect number of hyperparameters passed to Per Kernel "
+        "PerKernel requires exactly 3 hyperparameters with names 'gp_length*', 'gp_amp*', and 'gp_per*'."
 
         try:
             self.hparams['gp_length'].value
             self.hparams['gp_amp'].value
             self.hparams['gp_per'].value
-        except:
-            print("KERNEL ERROR: Per Kernel requires hyperparameters with string" +
-                  "names: 'gp_length*', 'gp_amp*', and 'gp_per*'")
+        except KeyError:
+            raise KeyError("PerKernel requires hyperparameters 'gp_length*', 'gp_amp*', and 'gp_per*'.")
+        except AttributeError:
+            raise AttributeError("PerKernel requires dictionary of Parameter objects as input")
 
     def __repr__(self):
         length= self.hparams['gp_length'].value
@@ -196,15 +204,17 @@ class QuasiPerKernel(Kernel):
                 self.hparams['gp_explength'] = hparams[par]
 
         assert len(hparams) == 4, \
-        "KERNEL ERROR: incorrect number of hyperparameters passed to QuasiPer Kernel "
+        "QuasiPerKernel requires exactly 4 hyperparameters with names 'gp_perlength*', 'gp_amp*', 'gp_per*', and 'gp_explength*'."
 
         try:
             self.hparams['gp_perlength'].value
             self.hparams['gp_amp'].value
             self.hparams['gp_per'].value
             self.hparams['gp_explength'].value
-        except:
-            "KERNEL ERROR: QuasiPer Kernel requires hyperparameters 'gp_perlength*', 'gp_amp*', 'gp_per*', and 'gp_explength*'"
+        except KeyError:
+            raise KeyError("QuasiPerKernel requires hyperparameters 'gp_perlength*', 'gp_amp*', 'gp_per*', and 'gp_explength*'.")
+        except AttributeError:
+            raise AttributeError("QuasiPerKernel requires dictionary of Parameter objects as input")
 
     def __repr__(self):
         perlength = self.hparams['gp_perlength'].value
@@ -257,9 +267,9 @@ class CeleriteKernel(Kernel):
     Args:
         hparams (dict of radvel.Parameter): dictionary containing
             radvel.Parameter objects that are GP hyperparameters
-            of this kernel. Must contain a multiple of 6 Parameters objects
-            with the following names:
-                a_real_k: (k is an integer) the 
+            of this kernel.
+
+        # TODO: better document string syntax for hyperparams.
     """
 
     @property
@@ -270,37 +280,65 @@ class CeleriteKernel(Kernel):
         # initialize celerite solver object
         self.solver = celerite.solver.CholeskySolver()
 
-        num_hparams = len(hparams)
+        # define number of real and number of complex terms
+        num_real_params = 0
+        num_complex_params = 0
+        print(hparams.keys())
+        for par in hparams.keys():
+            if 'Real' in par and int(par[0]) >= num_real_params:
+                num_real_params = int(par[0])
+            if 'Comp' in par and int(par[0]) >= num_complex_params:
+                num_complex_params = int(par[0])
 
-        # a_real should == c_real
-        self.a_real = np.zeros(num_hparams)
-        self.c_real = np.zeros(num_hparams)
-        self.a_comp = np.zeros(num_hparams)
-        self.b_comp = np.zeros(num_hparams)
-        self.c_comp = np.zeros(num_hparams)
-        self.d_comp = np.zeros(num_hparams)
+        self.aReal = np.zeros(num_real_params) + np.nan
+        self.cReal = np.zeros(num_real_params) + np.nan
+        self.aComp = np.zeros(num_complex_params) + np.nan
+        self.bComp = np.zeros(num_complex_params) + np.nan
+        self.cComp = np.zeros(num_complex_params) + np.nan
+        self.dComp = np.zeros(num_complex_params) + np.nan
 
         for par in hparams:
-            if par.startswith('a_real'):
-                self.a_real[int(par[-1])] = hparams[par].value
-            if par.startswith('c_real'):
-                self.c_real[int(par[-1])] = hparams[par].value
-            if par.startswith('a_comp'):
-                self.a_comp[int(par[-1])] = hparams[par].value
-            if par.startswith('b_comp'):
-                self.b_comp[int(par[-1])] = hparams[par].value
-            if par.startswith('c_comp'):
-                self.c_comp[int(par[-1])] = hparams[par].value
-            if par.startswith('d_comp'):
-                self.d_comp[int(par[-1])] = hparams[par].value
-          
+            try:
+                if 'aReal' in par:
+                    self.aReal[int(par[0]) - 1] = hparams[par].value
+                if 'cReal' in par:
+                    self.cReal[int(par[0]) - 1] = hparams[par].value
+                if 'aComp' in par:
+                    self.aComp[int(par[0]) - 1] = hparams[par].value
+                if 'bComp' in par:
+                    self.bComp[int(par[0]) - 1] = hparams[par].value
+                if 'cComp' in par:
+                    self.cComp[int(par[0]) - 1] = hparams[par].value
+                if 'dComp' in par:
+                    self.dComp[int(par[0]) - 1] = hparams[par].value
+            except IndexError:
+                print("Celerite Kernel requires an equal number of *_aReal* and *_cReal* hyperparameters," 
+                      + "and an equal number of *_aComp*, *_bComp*, *_cComp*, and *_dComp* hyperparameters.")
+
+        print(self.bComp)
+        print(self.aComp)
+        print(self.cComp)
+        print(self.dComp)
+        print(self.aReal)
+        print(self.cReal)
+        # TODO: test error messages
+        # TODO: modify test_api to test celerite kernel appropriately
+
+        assert np.nan not in \
+            np.array([self.aReal,self.cReal]).flatten(), \
+            "Celerite Kernel requires an equal number of *_aReal* and *_cReal* hyperparameters."
+
+        assert np.nan not in \
+            np.array([self.aComp,self.bComp,self.cComp,self.dComp]).flatten(), \
+            "Celerite Kernel requires an equal number of *_aComp*, *_bComp*, *_cComp*, and *_dComp* hyperparameters."
+
     def __repr__(self):
         msg = (
             "Celerite Kernel with real term coefficients: a = {}, c = {}"
             " and complex term coefficients: a = {}, b = {}, c = {}, d = {}."
         ).format(
-            self.a_real, self.c_real, self.a_comp, 
-            self.b_comp, self.c_comp, self.d_comp
+            self.aReal, self.cReal, self.aComp, 
+            self.bComp, self.cComp, self.dComp
           )
         return msg
 
@@ -321,12 +359,18 @@ class CeleriteKernel(Kernel):
         """
         self.solver.compute(
             self.params[self.jit_param].value, 
-            self.a_real, self.c_real, self.a_comp, self.b_comp, self.c_comp, self.d_comp,
+            self.aReal, self.cReal, self.aComp, self.bComp, self.cComp, self.dComp,
             self.A,self.U,self.V,self.dist,self.yerr**2
         )
         return self.solver
 
-    def add_diagonal_errors(cls, errors):
+    def add_diagonal_errors(self, errors):
         print("The celerite.solver.CholeskySolver() object adds errors along"
               + " the diagonal automatically. You should not need to use this"
               + " method with the CeleriteKernel.")
+
+
+if __name__ == "__main__":
+    params = {'gp_length_hires':  radvel.Parameter(value=1.), 'gp_length_harps':  radvel.Parameter(value=1.)}
+
+    a = SqExpKernel(params)
