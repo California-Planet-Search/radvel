1ad9aa2b8afcf1b0c3a0944daefd2b4e1a485480
BJ Fulton <bjfultn@gmail.com>
2018-02-15 15:16:57 -0800

    removing celerite requirement to test docs

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import sys
 import matplotlib
 matplotlib.use('agg')
 
-autodoc_mock_imports = ['_tkinter', 'pandas', 'scipy.optimize', 'celerite']
+autodoc_mock_imports = ['_tkinter', 'pandas', 'scipy.optimize', 'celerite', 'celerite.solver']
 for mod_name in autodoc_mock_imports:
     sys.modules[mod_name] = mock.Mock()
 
