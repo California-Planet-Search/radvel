4121900f92bd27d59e64f359577a083cca395129
BJ Fulton <bjfultn@gmail.com>
2019-02-13 08:08:50 -0800

    updated nbsphinx req

--- radvel/__init__.py
+++ radvel/__init__.py
@@ -25,7 +25,7 @@ def _custom_warningfmt(msg, *a, **b):
 __all__ = ['model', 'likelihood', 'posterior', 'mcmc', 'prior', 'utils',
          'fitting', 'report', 'cli', 'driver', 'gp']
 
-__version__ = '1.2.5'
+__version__ = '1.2.6'
 __spec__ = __name__
 __package__ = __path__[0]
 
