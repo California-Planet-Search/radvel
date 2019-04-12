440e4618b5b955ee8cc22b248734d2a2248e3010
Samuel Hadden <shadden1107@gmail.com>
2019-04-12 17:01:23 -0400

    fixed syntax error

--- radvel/posterior.py
+++ radvel/posterior.py
@@ -45,9 +45,8 @@ class Posterior(Likelihood):
         Returns:
             float: log probability of the likelihood + priors
         """
-
-       _logprob=0 
-       for prior in self.priors:
+        _logprob=0 
+        for prior in self.priors:
             _logprob += prior(self.params)
         if np.isfinite(_logprob):
             return _logprob + self.likelihood.logprob()
