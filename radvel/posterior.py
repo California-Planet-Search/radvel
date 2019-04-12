Fixed posterior class so that if priors evaluate to -inf, then likelihood calculation is skipped

radvel/posterior.py
@@ -46,10 +46,11 @@ class Posterior(Likelihood):
             float: log probability of the likelihood + priors
         """
 
-        _logprob = self.likelihood.logprob()
-        for prior in self.priors:
+       _logprob=0 
+       for prior in self.priors:
             _logprob += prior(self.params)
-
+        if np.isfinite(_logprob):
+            return _logprob + self.likelihood.logprob()
         return _logprob
 
     def logprob_array(self, param_values_array):