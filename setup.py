d43a98cbb5aaaa6d37b5c82f37e4a1bf70f9696c
BJ Fulton <bjfultn@gmail.com>
2023-08-10 15:52:58 -0600

    fixed blas_opt bug

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ class build_ext(_build_ext):
     def finalize_options(self):
         _build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
+        # __builtins__.__NUMPY_SETUP__ = False
         import numpy
         self.include_dirs.append(numpy.get_include())
 
